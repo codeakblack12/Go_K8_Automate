5ccd7e3ed8ef9f2b9a2f53505e1b3ec0826207d5
codeakblack12 <igbinedionpaul@gmail.com>
2026-04-17 10:50:33 -0700

    Fix IP error by setting a precise API server address

--- internal/config/defaults.go
+++ internal/config/defaults.go
@@ -3,7 +3,7 @@ package config
 const (
 	defaultOSFamily              = "ubuntu"
 	defaultKubernetesRepoVersion = "v1.35"
-	defaultAPIServerAddress      = "127.0.0.1"
+	defaultAPIServerAddress      = "192.168.1.20"
 	defaultPodNetworkCIDR        = "10.244.0.0/16"
 	defaultKubernetesVersion     = ""
 )
